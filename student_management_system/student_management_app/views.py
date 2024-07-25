89e6b17fb19d516cf7fa9078a03d396f7ee01c3d
binyul <jiibinyul@gmail.com>
2024-07-25 12:02:08 +0545

    initial commit

---
+++ student_management_system/student_management_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+
+
+# Create your views here.
+def showDemoPage(request):
+    return render(request,'demo.html')
+def showLoginPage(request):
+    return render(request,'login_page.html')
+def doLogin(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else: 
+        return HttpResponse("Email:"+request.POST.get("email")+" Password: "+request.POST.get("password"))
