61cb4dfddbb9d9b9c58db910800e702dbc308218
binyul <jiibinyul@gmail.com>
2024-07-27 13:54:36 +0545

    6th commit

--- student_management_system/student_management_system/urls.py
+++ student_management_system/student_management_system/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 
-from student_management_app import views
+from student_management_app import views,HodViews
 from student_management_system import settings
 
 urlpatterns = [
@@ -28,4 +28,6 @@ urlpatterns = [
     path('get_user_details',views.GetUserDetails),
     path('logout_user',views.logout_user),
     path('doLogin',views.doLogin),
+    path('admin_home',HodViews.admin_home),
+    path('add_staff',HodViews.add_staff),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
