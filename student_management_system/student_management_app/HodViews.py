45e7a686aec74c50a0908e87f9afefd0773b59ea
binyul <jiibinyul@gmail.com>
2024-07-28 14:36:22 +0545

    BE_addstaff

--- student_management_system/student_management_app/HodViews.py
+++ student_management_system/student_management_app/HodViews.py
@@ -6,3 +6,14 @@ def admin_home(request):
 
 def add_staff(request):
     return render(request,'hod_template/add_staff_template.html')
+
+def add_staff_save(request):
+    if request.method!="POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        first_name=request.POST.get('first_name')
+        first_name=request.POST.get('first_name')
+        username=request.POST.get('username')
+        address=request.POST.get('address')
