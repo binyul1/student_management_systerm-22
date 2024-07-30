db77b5899c642075f9c360aba9926e7f949303dd
binyul <jiibinyul@gmail.com>
2024-07-30 13:35:44 +0545

    remodel

--- student_management_system/student_management_app/HodViews.py
+++ student_management_system/student_management_app/HodViews.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+# from django.urls import reverse
 
 from student_management_app.models import CustomUser
 def admin_home(request):
@@ -17,9 +17,9 @@ def add_staff_save(request):
     else:
         first_name=request.POST.get('first_name')
         last_name=request.POST.get('last_name')
+        username=request.POST.get('username')
         email=request.POST.get('email')
         password=request.POST.get('password')
-        username=request.POST.get('username')
         address=request.POST.get('address')
         try:
             user=CustomUser.objects.create_user(username=username,password=password,email=email,last_name=last_name,first_name=first_name,user_type=2)
