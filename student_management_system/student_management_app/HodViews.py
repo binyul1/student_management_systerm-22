50e790668f5733ed8bc83b5d8ea4f1cf93e1af79
binyul <jiibinyul@gmail.com>
2024-07-30 16:49:09 +0545

    DB connected

--- student_management_system/student_management_app/HodViews.py
+++ student_management_system/student_management_app/HodViews.py
@@ -2,9 +2,10 @@ from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-# from django.urls import reverse
+from django.urls import reverse
 
-from student_management_app.models import CustomUser
+
+from student_management_app.models import CustomUser,staffs
 def admin_home(request):
     return render(request,'hod_template/home_content.html')
 
@@ -15,12 +16,12 @@ def add_staff_save(request):
     if request.method!="POST":
         return HttpResponse("Method Not Allowed")
     else:
-        first_name=request.POST.get('first_name')
-        last_name=request.POST.get('last_name')
-        username=request.POST.get('username')
-        email=request.POST.get('email')
-        password=request.POST.get('password')
-        address=request.POST.get('address')
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        address=request.POST.get("address")
         try:
             user=CustomUser.objects.create_user(username=username,password=password,email=email,last_name=last_name,first_name=first_name,user_type=2)
             user.staffs.address=address
