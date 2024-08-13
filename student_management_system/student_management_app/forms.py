168bed9a017bef45459b6f6c1b41b370ead16778
binyul <jiibinyul@gmail.com>
2024-08-13 15:04:05 +0545

    123

--- student_management_system/student_management_app/forms.py
+++ student_management_system/student_management_app/forms.py
@@ -5,12 +5,12 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 class AddStudentForm(forms.Form):
-    email=forms.EmailField(label="Email",max_length=50)
-    password=forms.CharField(label="Password",max_length=50,widget=forms.PasswordInput)
-    first_name=forms.CharField(label="First_name",max_length=50)
-    last_name=forms.CharField(label="Last_name",max_length=50)
-    username=forms.CharField(label="Username",max_length=50)
-    address=forms.CharField(label="Address",max_length=50)
+    email=forms.EmailField(label="Email",max_length=50,widget=forms.EmailInput(attrs={"class":"form-control"}))
+    password=forms.CharField(label="Password",max_length=50,widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    first_name=forms.CharField(label="First_name",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name=forms.CharField(label="Last_name",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
+    username=forms.CharField(label="Username",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
+    address=forms.CharField(label="Address",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     courses=Courses.objects.all()
     course_list=[]
     for course in courses:
@@ -22,8 +22,8 @@ class AddStudentForm(forms.Form):
         ("Female","Female"),
     )
     
-    course=forms.ChoiceField(label="Course",choices=course_list)
-    sex=forms.ChoiceField(label="Sex",choices=gender_choice)
-    session_start=forms.DateField(label="Session Start",widget=DateInput())
-    session_end=forms.DateField(label="Session End",widget=DateInput())
-    profile_pic=forms.FileField(label="Profile Pic")
+    course=forms.ChoiceField(label="Course",choices=course_list,widget=forms.Select(attrs={"class":"form-control"}))
+    sex=forms.ChoiceField(label="Sex",choices=gender_choice,widget=forms.Select(attrs={"class":"form-control"}))
+    session_start=forms.DateField(label="Session Start",widget=DateInput(attrs={"class":"form-control"}))
+    session_end=forms.DateField(label="Session End",widget=DateInput(attrs={"class":"form-control"}))
+    profile_pic = forms.FileField(label="Profile Pic", widget=forms.ClearableFileInput(attrs={"class": "form-control"}))
