7877d87a721948585f9a21d78830d8566dbbf954
binyul <jiibinyul@gmail.com>
2024-08-27 12:53:20 +0545

    restricted

--- student_management_system/student_management_app/forms.py
+++ student_management_system/student_management_app/forms.py
@@ -6,31 +6,41 @@ class DateInput(forms.DateInput):
     input_type = "date"
     
 
-
 class AddStudentForm(forms.Form):
     email = forms.EmailField(label = "Email", max_length=50,widget=forms.EmailInput(attrs={"class":"form-control"}))
     password = forms.CharField(label = "Password", max_length=50,widget = forms.PasswordInput(attrs={"class":"form-control"}))
     first_name = forms.CharField(label = "First Name", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     last_name = forms.CharField(label = "Last Name", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
-    username = forms.CharField(label = "Username", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
+    username = forms.CharField(label = "Usernam", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     address = forms.CharField(label = "Address", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
-
-    courses = Courses.objects.all()
     course_list = []
-    for course in courses:
-        small_course = (course.id,course.course_name)
-        course_list.append(small_course)
+    try:
+        courses = Courses.objects.all()
+
+        for course in courses:
+            small_course = (course.id,course.course_name)
+            course_list.append(small_course)
+    except:
+        course_list = []
+    # course_list = []
+
+    session_list = []
+    try:
+        sessions = SessionYearModel.object.all()
+        for ses in sessions:
+            small_ses = (ses.id,str(ses.session_start_year)+"  To  "+str(ses.session_end_year))
+            session_list.append(small_ses)
+    except:
+        session_list = []
 
     gender_choice = (
         ('Male',"Male"),
         ('Female',"Female"),
-
     ) 
 
     course = forms.ChoiceField(label = "Course",choices=course_list,widget=forms.Select(attrs={"class":"form-control"}) )
     sex = forms.ChoiceField(label = "Sex",choices=gender_choice,widget=forms.Select(attrs={"class":"form-control"}))
-    session_start = forms.DateField(label = "Session Start",widget = DateInput(attrs={"class":"form-control"}))
-    session_end = forms.DateField(label = "Session End",widget= DateInput(attrs={"class":"form-control"}))
+    session_year_id = forms.ChoiceField(label = "Session Year",widget = forms.Select(attrs={"class":"form-control"}),choices=session_list)
     profile_pic = forms.FileField(label = "Profile Pic", max_length=50,widget=forms.FileInput(attrs={"class":"form-control"}))
 
 class EditStudentForm(forms.Form):
@@ -40,11 +50,29 @@ class EditStudentForm(forms.Form):
     username = forms.CharField(label = "Usernam", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     address = forms.CharField(label = "Address", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
 
-    courses = Courses.objects.all()
     course_list = []
-    for course in courses:
-        small_course = (course.id,course.course_name)
-        course_list.append(small_course)
+    try:
+        courses = Courses.objects.all()
+        for course in Courses:
+            small_course = (course.id,course.course_name)
+            course_list.append(small_course)
+
+    except:
+        course_list=[]
+    
+        
+    session_list = []
+    try:
+
+
+        sessions = SessionYearModel.object.all()
+
+        for ses in sessions:
+            small_ses = (ses.id,str(ses.session_start_year)+"  To  "+str(ses.session_end_year))
+            session_list.append(small_ses)
+
+    except:
+        pass
 
     gender_choice = (
         ('Male',"Male"),
--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -3,17 +3,16 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
+# Create your models here.
 class CustomUser(AbstractUser):
     user_type_data=((1,"HOD"),(2,"Staff"),(3,"Student"))
     user_type=models.CharField(default=1,choices=user_type_data,max_length=10)
-
 
 class AdminHOD(models.Model):
     id=models.AutoField(primary_key=True)
     admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
 
 class Staffs(models.Model):
@@ -33,16 +32,16 @@ class Courses(models.Model):
 
 class Subjects(models.Model):
     id=models.AutoField(primary_key=True)
-    subject_name=models.CharField(max_length=255)     
+    subject_name=models.CharField(max_length=255)
     course_id=models.ForeignKey(Courses,on_delete=models.CASCADE,default=1)
-    staff_id = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    staff_id=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
 
 class Students(models.Model):
     id=models.AutoField(primary_key=True)
-    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)   
+    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     gender=models.CharField(max_length=255)
     profile_pic=models.FileField()
     address=models.TextField()
@@ -128,6 +127,7 @@ class NotificationStaffs(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
+
 @receiver(post_save,sender=CustomUser)
 def create_user_profile(sender,instance,created,**kwargs):
     if created:
@@ -136,7 +136,7 @@ def create_user_profile(sender,instance,created,**kwargs):
         if instance.user_type==2:
             Staffs.objects.create(admin=instance,address="")
         if instance.user_type==3:
-            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_start_year="2020-01-01",session_end_year="2021-01-01",address ="",profile_pic="",gender="")
+            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_start_year="2020-01-01",session_end_year="2021-01-01",address="",profile_pic="",gender="")
 
 @receiver(post_save,sender=CustomUser)
 def save_user_profile(sender,instance,**kwargs):
